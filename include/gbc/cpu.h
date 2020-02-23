edb4d9cc979518fafc89a7ded1b8932c6b77fe52
Christian Nieves <christian@nieves.io>
2020-02-23 15:19:08 -0700

    gbc/cpu: fix typedefs

--- include/gbc/cpu.h
+++ include/gbc/cpu.h
@@ -1,40 +1,42 @@
-#ifndef CPU_H
-#define CPU_H
+#pragma once
 #include "gbc/mmu.h"
 
 typedef struct {
     // two types of timeclocks in the Z80
-    struct {
-        int m;
-        int t;
-    } clk;
+    int m;
+    int t;
+} gbc_clock;
 
-    /*
-     * 16bit Hi   Lo   Name/Function
-     * AF    A    -    Accumulator & Flags
-     * BC    B    C    BC
-     * DE    D    E    DE
-     * HL    H    L    HL
-     * SP    -    -    Stack Pointer
-     * PC    -    -    Program Counter/Pointer
-     * As shown above, most registers can be accessed either as one 16bit
-     * register, or as two separate 8bit registers.
-     */
-    struct {
-        u16 pc, sp;
-        u8 a, b, c, d, e, h, l, f; // 8-bit registers
-        //u8 m, t; // Clock for last instruction
-    } registers;
+/*
+ * 16bit Hi   Lo   Name/Function
+ * AF    A    -    Accumulator & Flags
+ * BC    B    C    BC
+ * DE    D    E    DE
+ * HL    H    L    HL
+ * SP    -    -    Stack Pointer
+ * PC    -    -    Program Counter/Pointer
+ * As shown above, most registers can be accessed either as one 16bit
+ * register, or as two separate 8bit registers.
+ */
+typedef struct {
+    u16 pc, sp;
+    u8 a, b, c, d, e, h, l, f; // 8-bit registers
+    //u8 t, m; // Clock for last instruction
+    gbc_clock clk;
+} gbc_cpu_registers;
 
-    struct {
-        u8 a, b, c, d, e, h, l, f;
-    } rsv; // saved register set
+// save register set
+typedef struct {
+    u8 a, b, c, d, e, h, l, f;
+} gbc_cpu_rsv;
+
+typedef struct {
+    gbc_clock clk;
+    gbc_cpu_registers registers;
+    gbc_cpu_rsv rsv;
 
     gbc_mmu *mmu;
-
 } gbc_cpu;
 
 void gbc_cpu_reset(gbc_cpu *cpu);
 void gbc_cpu_loop(gbc_cpu *cpu);
-
-#endif
--- source/gbc/cpu.c
+++ source/gbc/cpu.c
@@ -1,30 +1,23 @@
 #include "gbc/cpu.h"
-#include "gbc/mmu.h"
 #include "gbc/ops.h"
 
-#include "font.h"
-#include "text.h"
-#include "cpu.h"
-
-#define BIT_RANGE(x, lsb, msb) (x >> lsb) & ~(~0 << (msb-lsb+1))
-
 void gbc_cpu_reset(gbc_cpu *cpu) {
-    cpu.registers.a = 0;
-    cpu.registers.b = 0;
-    cpu.registers.c = 0;
-    cpu.registers.d = 0;
-    cpu.registers.e = 0;
-    cpu.registers.h = 0;
-    cpu.registers.l = 0;
-    cpu.registers.f = 0;
+    cpu->registers.a = 0;
+    cpu->registers.b = 0;
+    cpu->registers.c = 0;
+    cpu->registers.d = 0;
+    cpu->registers.e = 0;
+    cpu->registers.h = 0;
+    cpu->registers.l = 0;
+    cpu->registers.f = 0;
 
-    cpu.registers.pc = 0;
-    cpu.registers.sp = 0;
-    cpu.registers.clk.m = 0;
-    cpu.registers.clk.t = 0;
+    cpu->registers.pc = 0;
+    cpu->registers.sp = 0;
+    cpu->registers.clk.m = 0;
+    cpu->registers.clk.t = 0;
 
-    cpu.clk.m = 0;
-    cpu.clk.t = 0;
+    cpu->clk.m = 0;
+    cpu->clk.t = 0;
 }
 
 void gbc_cpu_loop(gbc_cpu *cpu) {
@@ -35,43 +28,15 @@ void gbc_cpu_loop(gbc_cpu *cpu) {
         // Fetch and execute instruction
         u8 opcode = read_u8(cpu->mmu, cpu->registers.pc++);
         OPS[opcode](cpu);
-        char s[80];
-        sprintf(s, "%x", opcode);
-        put_s(i++, s);
-        if (i > 12) {
-            i = 0;
-        }
+        /*char s[80];*/
+        /*sprintf(s, "%x", opcode);*/
+        /*put_s(i++, s);*/
+        /*if (i > 12) {*/
+            /*i = 0;*/
+        /*}*/
 
         // Add execution time to the CPU clk
-        cpu->clk.m += cpu->registers.m;
-        cpu->clk.t += cpu->registers.t;
+        cpu->clk.m += cpu->registers.clk.m;
+        cpu->clk.t += cpu->registers.clk.t;
     }
-}
-
-// x = the opcode's 1st octal digit (i.e. bits 7-6)
-// y = the opcode's 2nd octal digit (i.e. bits 5-3)
-// z = the opcode's 3rd octal digit (i.e. bits 2-0)
-// p = y rightshifted one position (i.e. bits 5-4)
-// q = y modulo 2 (i.e. bit 3)
-// implementation of the decode strategy descrubed here:
-// https://gb-archive.github.io/salvage/decoding_gbz80_opcodes/Decoding%20Gamboy%20Z80%20Opcodes.html
-void execute_smart(gbc_cpu *cpu, u8 opcode) {
-    x = BIT_RANGE(opcode, 6, 7);
-    y = BIT_RANGE(opcode, 3, 5);
-    z = BIT_RANGE(opcode, 0, 2);
-    p = BIT_RANGE(opcode, 4, 5);
-    q - y % 3;
-
-    switch (x) {
-        case 0:
-            switch (z) {
-                case 0: relative_jumps_and_assorted_ops(y)
-                case 1: (y)
-            }
-            break;
-    }
-}
-
-void relative_jumps_and_assorted_ops(u8 y) {
-
 }
