5d904ef336b69ec01713cdffaaba8f1ce34597e3
Christian Nieves <christian@nieves.io>
2020-02-24 20:30:32 -0700

    gbc/cpu: add test CPU loop and set_boot_state

--- source/gbc_cpu.c
+++ source/gbc_cpu.c
@@ -3,6 +3,7 @@
 
 #include "gbc_cpu.h"
 #include "gbc_ops.h"
+#include "gbc_io.h"
 #include "text.h"
 
 void gbc_cpu_reset(gbc_cpu *cpu) {
@@ -24,22 +25,47 @@ void gbc_cpu_reset(gbc_cpu *cpu) {
     cpu->clk.t = 0;
 }
 
+void gbc_cpu_set_boot_state(gbc_cpu *cpu) {
+    cpu->registers.pc = 0x100;
+    cpu->registers.a = 0x01;
+    cpu->registers.f = 0xB0;
+    cpu->registers.b = 0x00;
+    cpu->registers.c = 0x13;
+    cpu->registers.d = 0x00;
+    cpu->registers.e = 0xD8;
+    cpu->registers.h = 0x01;
+    cpu->registers.l = 0x4D;
+    cpu->registers.sp = 0xFFFE;
+
+    cpu->mmu->in_bios = false;
+}
+
 void gbc_cpu_loop(gbc_cpu *cpu) {
     execute_init(cpu);
 
     char s[80];
-    int max = 5;
+    int max = 100;
     int instr = 0;
+    /*sprintf(s, "LCDCONT: %x", get_address_ptr(cpu->mmu, IO_LCDCONT));*/
+    /*put_l(s);*/
+    /*sprintf(s, "ROM: %x", &cpu->mmu->rom);*/
+    /*put_l(s);*/
+
     while(1) {
         instr++;
         // Fetch and execute instruction
         u8 opcode = read_u8(cpu->mmu, cpu->registers.pc++);
+        for(u8 i=0; i<60; i++) {VBlankIntrWait();}
         void (*funcPtr)(gbc_cpu*) = *OPS[opcode];
         (funcPtr)(cpu);
 
-        /*if (instr % 10000 == 0) {*/
+        /*if (instr % 101 == 0) {*/
+            sprintf(s, "op: %x    &fb:%x    fb: %x", opcode, &cpu->gpu->fb, cpu->gpu->fb[0]);
+            put_l(s);
+        /*}*/
+
+        /*if (instr % 100 == 0) {*/
             /*clear();*/
-            /*sprintf(s, "I#:%d     PC: %d", instr, cpu->registers.pc);*/
             /*sprintf(s, "OP: %x", opcode);*/
             /*put_l(s);*/
             /*sprintf(s, "A: %x    B: %x    C: %x", cpu->registers.a, cpu->registers.b, cpu->registers.c);*/
@@ -50,6 +76,10 @@ void gbc_cpu_loop(gbc_cpu *cpu) {
             /*put_l(s);*/
             /*sprintf(s, "T: %x    SP: %x    PC: %x", cpu->registers.clk.t, cpu->registers.sp, cpu->registers.pc);*/
             /*put_l(s);*/
+            /*sprintf(s, "ROM: %x", &cpu->mmu->rom);*/
+            /*put_l(s);*/
+            /*sprintf(s, "LCDCONT: %x", read_u8(cpu->mmu, IO_LCDCONT));*/
+            /*put_l(s);*/
         /*}*/
 
         /*put_c(screen_line-1, 5, 'E');*/
@@ -57,7 +87,6 @@ void gbc_cpu_loop(gbc_cpu *cpu) {
         // Add execution time to the CPU clk
         cpu->clk.m += cpu->registers.clk.m;
         cpu->clk.t += cpu->registers.clk.t;
-        /*VBlankIntrWait();*/
         gpu_start_frame(cpu->gpu);
     }
 }
--- source/main.c
+++ source/main.c
@@ -28,7 +28,7 @@ int main(void) {
     // set screen H and V scroll positions
     BG_OFFSET[0].x = 0; BG_OFFSET[0].y = 0;
 
-    /*put_s(0, "TEMW - this emu might work");*/
+    put_l("TEMW - this emu might work");
     /*put_s(0, "TEMW - this emu might work");*/
     /*put_s(1, "--------------------------------");*/
     // set the screen base to 31 (0x600F800) and char base to 0 (0x6000000)
@@ -36,9 +36,6 @@ int main(void) {
 
     // screen mode & background to display
     SetMode( MODE_0 | BG0_ON );
+    /*SetMode( MODE_4 | BG3_ON );*/
     emu_run();
-
-    while (1) {
-        VBlankIntrWait();
-    }
 }
