b5e6bd8ca26c017ac41a4c3c74d80cefa5461a17
Christian Nieves <christian@nieves.io>
2020-02-24 17:36:27 -0700

    add gbc_io.h for io registers

--- include/gbc_io.h
+++ include/gbc_io.h
@@ -52,3 +52,73 @@
 #define IO_SNDREG50 0xFF24
 #define IO_SNDREG51 0xFF25
 #define IO_SNDREG52 0xFF26
+
+// LCDCONT
+#define OPT_Window_Tile_Map_0     0
+#define OPT_Window_Tile_Map_1     1
+#define OPT_BGWindow_Tile_Data_0  0
+#define OPT_BGWindow_Tile_Data_1  1
+#define OPT_BG_Tile_Map_0         0
+#define OPT_BG_Tile_Map_1         1
+#define OPT_OBJ_Size_8x8                  0
+#define OPT_OBJ_Size_8x16                 1
+
+
+// OBJ
+#define OPT_OBJ_Flag_priority (0x01 << 7)
+#define OPT_OBJ_Flag_yflip    (0x01 << 6)
+#define OPT_OBJ_Flag_xflip    (0x01 << 5)
+#define OPT_OBJ_Flag_palette  (0x01 << 4)
+
+#define OPT_LYC_NEQ_LY    0
+#define OPT_LYC_EQ_LY     1
+#define OPT_MODE_HBLANK   0
+#define OPT_MODE_VBLANK   1
+#define OPT_MODE_OAM      2
+#define OPT_MODE_OAM_VRAM 3
+
+#define MASK_IO_BGRDPAL_Shade_Color_3 ((0x01 << 7) + (0x01 << 6))
+#define MASK_IO_BGRDPAL_Shade_Color_2 ((0x01 << 5) + (0x01 << 4))
+#define MASK_IO_BGRDPAL_Shade_Color_1 ((0x01 << 3) + (0x01 << 2))
+#define MASK_IO_BGRDPAL_Shade_Color_0 ((0x01 << 1) + (0x01 << 0))
+
+#define OPT_Color_White      0
+#define OPT_Color_Light_Gray 1
+#define OPT_Color_Dark_Gray  2
+#define OPT_Color_Black      3
+
+#define MASK_INT_High_to_Low_P10_P13    (0x01 << 4)
+#define MASK_INT_End_Serial_IO_Transfer (0x01 << 3)
+#define MASK_INT_Timer_Overflow         (0x01 << 2)
+
+#define MASK_IO_SIOCONT_Start_Flag (0x01 << 7)
+#define MASK_IO_SIOCONT_IO_Select  (0x01 << 0)
+
+#define MASK_IO_TIMCONT_Start  (0x01 << 2)
+#define MASK_IO_TIMCONT_clock ((0x01 << 1) + (0x01 << 0))
+
+#define OPT_Timer_clock_4096   0
+#define OPT_Timer_clock_262144 1
+#define OPT_Timer_clock_65536  2
+#define OPT_Timer_clock_16384  3
+
+// from: https://github.com/Dhole/miniBoy/blob/461697a267648391fe78897ac83daa54df080810/io_regs.h
+#define MASK_LCDCONT_LCD_Display_Enable             (0x01 << 7)
+#define MASK_LCDCONT_Window_Tile_Map_Display_Select (0x01 << 6)
+#define MASK_LCDCONT_Window_Display_Enable          (0x01 << 5)
+#define MASK_LCDCONT_BGWindow_Tile_Data_Select      (0x01 << 4)
+#define MASK_LCDCONT_BG_Tile_Map_Display_Select     (0x01 << 3)
+#define MASK_LCDCONT_OBJ_Size                       (0x01 << 2)
+#define MASK_LCDCONT_OBJ_Display_Enable             (0x01 << 1)
+#define MASK_LCDCONT_BG_Display_Enable              (0x01 << 0)
+
+#define MASK_INT_LCDSTAT_INT            (0x01 << 1)
+#define MASK_INT_VBLANK                 (0x01 << 0)
+
+#define MASK_LCDSTAT_LYC_LY_COINCIDENCE_INTERRUPT (0x01 << 6)
+#define MASK_LCDSTAT_MODE_2_OAM_INTERRUPT         (0x01 << 5)
+#define MASK_LCDSTAT_MODE_1_VBLANK_INTERRUPT      (0x01 << 4)
+#define MASK_LCDSTAT_MODE_0_HBLANK_INTERRUPT      (0x01 << 3)
+#define MASK_LCDSTAT_COINCIDENCE_FLAG             (0x01 << 2)
+#define MASK_LCDSTAT_MODE_FLAG                   ((0x01 << 1) + (0x01 << 0))
+
